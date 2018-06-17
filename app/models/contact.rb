a0e03a588b949ac07aecaf16a220b82dccac1c32
ptavarez16 <ptavarez16@yahoo.com>
2018-06-17 12:45:10 -0400

    Include PgSearch
    - Include PGSearch to search against all contact columns

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Contact < ApplicationRecord
+  include PgSearch
+
+  pg_search_scope :search_for,
+                  against: %i[first_name last_name job_title phone_number email],
+                  using: { tsearch: { any_word: true, prefix: true } }
 end
